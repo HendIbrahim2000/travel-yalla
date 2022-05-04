13f2af0f93644955d4e7a59fb50729137df92d27
HendIbrahim2000 <hendibrahim2000@gmail.com>
2022-05-04 23:22:39 +0200

    Testimonial section & Top hotels and airlines section

--- js/main.js
+++ js/main.js
@@ -210,5 +210,39 @@ if ($("#datepicker-checkout4").length) {
     $("#datepicker-checkout4").datepicker("setDate", "0");
 }
 
+// Testimonial Slider
+
+$('.testimonial-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.test-prev'),
+    nextArrow: $('.test-next'),
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+
 })(jQuery);
 
